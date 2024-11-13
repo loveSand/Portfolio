9fc0f53c98f339560f9f43d10c702d85d908e178
nas <r.jonas.p22@gmail.com>
2024-11-13 23:09:01 +0800

    fixing arrangements

--- assets/scripts/gulpfile.js
+++ assets/scripts/gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const notify = require('gulp-notify');
 
 gulp.task('sass', function() {
     return gulp.src('./assets/style/main.scss')
@@ -11,7 +13,15 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('.assets/style/**/*.scss', gulp.series('sass'));
+    gulp.watch('.assets/style/**/*.scss', gulp.series('sass'))
+    .pipe(notify('Sass file compiled!'));
 })
+
+gulp.task('clear-cache', function() {
+    return gulp.src('.assets/style/**/*.scss')
+    .pipe(sass({ cacheClear: true}))
+    .pipe(clean())
+    .pipe(gulp.dest('.'));
+});
 
 gulp.task('default', gulp.series('sass', 'watch'));
