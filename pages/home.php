3fe0097cc33c3c272446995f737722f7c955ba1c
nas <r.jonas.p22@gmail.com>
2024-11-15 20:02:11 +0800

    updates

--- pages/home.php
+++ pages/home.php
@@ -2,8 +2,8 @@
     <div class="hero">
         <div class="hero-content">
             <h1>Welcome to my Portfolio</h1>
-            <p>Hi, I'm jonas, a beginner web developer</p>
-            <a href="index.php?page=projects" class="hero-btn">My Work</a>
+            <p>Beginner | Front-End | Back-End</p>
+            <a href="?page=projects" class="btn btn-primary">My Work</a>
         </div>
     </div>
 </main>
